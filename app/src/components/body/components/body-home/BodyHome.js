aa8f5c8259529d1e403314a81de1b5341b026061
Jacob Cunningham <jcun4128@gmail.com>
2024-05-15 15:55:16 -0500

    home view

--- app/src/components/body/components/body-home/BodyHome.js
+++ app/src/components/body/components/body-home/BodyHome.js
@@ -8,13 +8,23 @@ const BodyHome = () => {
   const bills = getBills();
   const cash = getCash();
 
+  const getSum = (arr) => {
+    let sum = 0;
+    arr.forEach(source => sum += parseFloat(source.amount));
+    return sum;
+  }
+
   return (
     <div className="app__body-home">
       <div className="app__body-home-bills">
-        {renderBills(bills)}
+        <h2>Bills</h2>
+        <div className="app__body-home-bill-rows">{renderBills(bills)}</div>
+        <h3>Total ${getSum(bills)}</h3>
       </div>
       <div className="app__body-home-cash">
-        {renderCash(cash)}
+        <h2>Cash</h2>
+        <div className="app__body-home-cash-rows">{renderCash(cash)}</div>
+        <h3>Total ${getSum(cash)}</h3>
       </div>
     </div>
   );
